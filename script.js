c31056523881410128c9767252ece57667aa8f8e
Andrea Francesca Bellia <76186697+andie3005@users.noreply.github.com>
2023-05-29 15:53:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 # optional to enable smooth scrolling to the navigation links    
 
-#document.addEventListener('DOMContentLoaded', () => {
-    #const navLinks = document.querySelectorAll('nav ul li a');
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
     
-    #for (const link of navLinks) {
-        #link.addEventListener('click', (e) => {
-            #e.preventDefault();
-            #const targetId = link.getAttribute('href');
-            #const targetElement = document.querySelector(targetId);
-            #targetElement.scrollIntoView({ behavior: 'smooth' });
+    for (const link of navLinks) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
         });
     }
 });
