14bb51f5c512f1eb1fe79b7df5f8b518724ee9fd
Andrea Francesca Bellia <76186697+andie3005@users.noreply.github.com>
2023-05-29 15:24:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    for (const link of navLinks) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
