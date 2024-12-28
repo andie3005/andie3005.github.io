98be84756ef717f2f610e4b108c2a9232e66caf2
Andrea Francesca Bellia <76186697+andie3005@users.noreply.github.com>
2024-12-28 03:38:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,19 +25,3 @@ window.onload = loadHeaderAndFooter;
 // Additional functionalities can go here, like form validation, event handling, etc.
 
 
-
-
-# optional to enable smooth scrolling to the navigation links    
-
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    
-    for (const link of navLinks) {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const targetId = link.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            /*targetElement.scrollIntoView({ behavior: 'smooth' });*/
-        });
-    }
-});
